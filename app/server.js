9ed1bb13eae294a1e1e17a638e392055b8a47e50
yashweblife <yashweblife20@gmail.com>
2023-06-05 16:47:22 -0500

    add basic code

--- app/server.js
+++ app/server.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let express = require("express")
+let path = require("path")
+let fs = require("fs")
+let bodyParser = require("body-parser")
+let mongo = require("mongodb").MongoClient;
+
+let app = express();
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json())
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+
+app.listen(3000, ()=>{
+    
+})
