bbe1d9352718b3cd314b27b1caa3277e0c702275
yashweblife <yashweblife20@gmail.com>
2023-06-06 02:58:23 -0500

    add endpoint response

--- src/server.js
+++ src/server.js
@@ -7,15 +7,19 @@ let mongo = require("mongodb").MongoClient;
 let app = express();
 
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 app.use(bodyParser.json())
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
+app.get('/todolists',(req, res)=>{
+    res.send({
+        base:"Hello World"
+    })
+})
 
-
-app.listen(3000, ()=>{
-   console.log("Running on port:", 3000) 
+app.listen(3000, () => {
+    console.log("Running on port:", 3000)
 })
